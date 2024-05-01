08c10cb3fc767eb5392e77607de6ec96c3793db9
ElanchezhianShanmugam <166918539+ElanchezhianShanmugam@users.noreply.github.com>
2024-05-01 15:27:25 +0530

    project1.2

--- project1.c
+++ project1.c
@@ -3,5 +3,7 @@ int main()
 {
     printf("Welcome to C Programming \n");
     printf("C is a Procedural Programming Language \n");
+    printf("C is the fundamental programming language and base for other programming language .");
+    printf("C language was developed by Dennis Ritchie at the Bell Laboratories in 1972 .");
     return 0;
 }
